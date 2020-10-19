64ebe184b29bbecd332a7a3274a7230e970606f3
johnson_lin0930 <johnson870930@gmail.com>
2020-10-19 20:25:27 +0800

    init

---
+++ src/components/TodoList.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+        <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+            <div class="todo-item-left">
+                <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label">
+                    {{todo.title}}
+                </div>
+                <input v-else class="todo-item-edit" type="text" v-model="todo.title">
+            </div>
+            <div class="remove-item" @click="removeTodo(index)">
+                &times;
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  props: {
+    msg: String
+  },
+  data(){
+      return {
+        newTodo: "",
+        idForTodo: 3,
+        todos:[
+            {
+            'id':1,
+            'title':"Finish Vue Screencast",
+            'complete':false,
+            },
+            {
+            'id':2,
+            'title':"Take over world",
+            'complete':false,
+            },
+        ]
+      }
+  },
+  methods: {
+      addTodo(){
+          if(this.newTodo.trim().length == 0){
+              return
+          }
+          this.todos.push({
+              id: this.idForTodo,
+              title: this.newTodo,
+              completed: false,
+          })
+
+          this.newTodo = ""
+          this.idForTodo++
+      },
+      editTodo(todo){
+          alert("double clicked")
+          todo.editing = true
+      },
+      removeTodo(index){
+          this.todos.splice(index, 1)
+      }
+  }
+}
+</script>
+
+<style lang="scss">
+    .todo-input {
+        width: 100%;
+        padding: 10px 18px;
+        font-size: 18px;
+        margin-bottom: 16px;
+
+        &:focus {
+            outline: 0;
+        }
+    }
+
+    .todo-item {
+        margin-bottom: 12px;
+        display: flex;
+        align-items: center;
+        justify-content: "space-between";
+    }
+
+    .remove-item {
+        cursor: pointer;
+        margin-left:14px;
+        &:hover {
+            color: block;
+        }
+    }
+
+    .todo-item-left {
+        display: flex;
+        align-items: center;
+    }
+
+    .todo-item-label {
+        font-size: 24px;
+        color: #2c3e50;
+        margin-left: 12px;
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        font-family: 'Avenir', Arial, Helvetica, sans-serif;
+
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
